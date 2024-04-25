fc16e43bfc6552bdc0ef218f3d54b6a1e01a36e3
Truong Tuan Loc <Loctruongtuan@gmail.com>
2024-04-25 14:13:43 +0700

    update

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -79,7 +79,7 @@ export default function Header() {
       <div className="hidden md:flex items-center justify-between">
         <nav className="flex items-center gap-8 text-gray-500 font-semibold">
           <Link className="text-primary font-semibold text-2xl" href={'/'}>
-            ST PIZZA
+            Food Lover
           </Link>
           <Link href={'/'}>Home</Link>
           <Link href={'/menu'}>Menu</Link>
