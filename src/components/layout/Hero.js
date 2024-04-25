f1cc064c663b12fcca284074e37f9574671db6e2
Truong Tuan Loc <Loctruongtuan@gmail.com>
2024-04-25 14:51:34 +0700

    update

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,21 +6,23 @@ export default function Hero() {
     <section className="hero md:mt-4">
       <div className="py-8 md:py-12">
         <h1 className="text-4xl font-semibold">
-          Everything<br />
-          is better<br />
+          Everything
+          <br />
+          is better
+          <br />
           with&nbsp;
-          <span className="text-primary">
-            Food
-          </span>
+          <span className="text-primary">Food</span>
         </h1>
         <p className="my-6 text-gray-500 text-sm">
-          Enjoy a wide range of delicious meals, from the comfort of your home.
+          Discover the best dishes in town and enjoy a culinary journey like no
+          other.
         </p>
         <div className="flex gap-4 text-sm">
-          <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
-            Order now
+          <button className="justify-center uppercase items-center bg-primary gap-2 text-white px-4 py-2 rounded-full">
+            <a href="/menu">Order now</a>
             <Right />
           </button>
+
           <button className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold">
             Learn more
             <Right />
@@ -28,8 +30,14 @@ export default function Hero() {
         </div>
       </div>
       <div className="relative hidden md:block">
-        <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={'pizza'} />
+        <Image
+          src={"/pizza.png"}
+          layout={"fill"}
+          objectFit={"contain"}
+          alt={"pizza"}
+        />
       </div>
     </section>
   );
 }
+
